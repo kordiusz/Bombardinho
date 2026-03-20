1645b217bd79a6e7969e7215e04c04fa8a87e575
Hedzik <igorkosakowski12@gmail.com>
2026-03-20 19:40:28 +0100

    prepare for heroku env.

--- server.js
+++ server.js
@@ -430,7 +430,8 @@ io.on("connection", (socket) => {
 // server.listen(3000, () => {
 //     console.log("Server listening on http://localhost:3000");
 // });
-
-server.listen(5678, '0.0.0.0', () => {
-  console.log("Server listening on http://0.0.0.0:5678");
+const PORT = process.env.PORT || 3000;
+app.use(express.static(path.join(__dirname, 'public')));
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server listening on http://0.0.0.0:${PORT}`);
 });
